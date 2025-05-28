d710df048a3555e39cff0a4c9a7ba13fd4386ad3
Yash Agarwal <yashagarwal8923@gmail.com>
2025-05-28 14:58:32 +0530

    Fix: use default SQLite and support PostgreSQL for Render

--- stock_prediction/stock_prediction/settings.py
+++ stock_prediction/stock_prediction/settings.py
@@ -88,14 +88,22 @@ WSGI_APPLICATION = 'stock_prediction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-DATABASES['default']=dj_database_url.config(conn_max_age=600,ssl_require=True)
+# DATABASES['default']=dj_database_url.config(conn_max_age=600,ssl_require=True)
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
+}
 
 # Email Configuration for Password Reset
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Change this for real emails
